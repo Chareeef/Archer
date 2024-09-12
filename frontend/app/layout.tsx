28f5f5c50d51eaf08cfa96c5dd6b96b78b6a3988
Chareeef <youssef.charif.h@gmail.com>
2024-09-12 18:37:31 +0000

    Correct vercel analytics import

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { AuthProvider } from "@/context/AuthContext";
 import { AlertProvider } from "@/context/AlertContext";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
